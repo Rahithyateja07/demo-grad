4233e33eeccdc007d86f65fa7ea84b930eae4306
Ambarapu Rahitya Teja <108253538+Rahithyateja07@users.noreply.github.com>
2024-02-28 15:58:39 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -1,5 +1,6 @@
 public class Abc{
     public static void main(String[] args) {
         System.out.println("Have a good day");
+        System.out.println("This is the webhook integration");
     }
 }
